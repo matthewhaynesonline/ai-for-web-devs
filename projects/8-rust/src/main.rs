33420438ea545026fb9e0a0eaeaea77e62b9d608
Matthew Haynes <70829360+matthewhaynesonline@users.noreply.github.com>
2025-06-29 15:34:39 -0400

    Add cli args for flexibility

--- projects/8-rust/src/main.rs
+++ projects/8-rust/src/main.rs
@@ -1,14 +1,29 @@
 use anyhow::{Context, Result};
 use axum::{Router, routing::get};
+use clap::Parser;
 use tracing::info;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// IP to serve on.
+    #[arg(long, default_value_t = String::from("0.0.0.0"))]
+    ip: String,
+
+    /// Port to serve on.
+    #[arg(short, long, default_value_t = 3000)]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Args::parse();
+
     init_logging();
 
     let app = Router::new().route("/", get(root));
 
-    let addr = "0.0.0.0:3000";
+    let addr = format!("{}:{}", args.ip, args.port);
     let listener = tokio::net::TcpListener::bind(&addr)
         .await
         .context(format!("Could not bind TCP Listener on {addr}"))?;
